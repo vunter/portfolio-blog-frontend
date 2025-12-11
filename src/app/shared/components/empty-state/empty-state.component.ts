aeecda98aa2dd1bb51e1beb40c89f1b5bb140de1
Leonardo Catananti <leonardo.catananti@gmail.com>
2025-12-10 21:12:48 -0300

    feat(shared): add empty state component

---
+++ src/app/shared/components/empty-state/empty-state.component.ts
@@ -0,0 +1,13 @@
+import { Component, input, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-state',
+  imports: [],
+  templateUrl: './empty-state.component.html',
+  styleUrl: './empty-state.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EmptyStateComponent {
+  title = input<string>('');
+  message = input<string>('');
+}
