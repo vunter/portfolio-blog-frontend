ae6d7295d438fca56ec081721e291a46d98b7ffb
Leonardo Catananti <leonardo.catananti@gmail.com>
2026-01-07 20:08:47 -0300

    feat(pages): add about, privacy, and terms pages

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { I18nService } from '../../core/services/i18n.service';
+import { PublicProfileService } from '../../core/services/public-profile.service';
+import { SeoService } from '../../core/services/seo.service';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutComponent implements OnInit {
+  i18n = inject(I18nService);
+  profileService = inject(PublicProfileService);
+  private seo = inject(SeoService);
+
+  ngOnInit(): void {
+    this.seo.update({
+      title: this.i18n.t('about.title'),
+      description: this.i18n.t('seo.about.description'),
+      url: '/about',
+      type: 'website',
+      locale: this.seo.getLocale(this.i18n.language()),
+    });
+  }
+}
